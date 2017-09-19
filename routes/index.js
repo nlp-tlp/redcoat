0f8170518ecb0d7fa4d1f0f8bc2f1cc0d05e6f0b
Michael-Stewart-Webdev <michael.stewart.webdev@gmail.com>
2017-09-20 00:51:59 +0800

    made homepage and began working on form

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+
+	
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var extend = require('util')._extend
+//const fileType = require('file-type');
+
+
+
+function buildRoute(path, action, variables) {
+	router.get(path, function(req, res, next) {
+		res.render(action, extend(variables, {path: req.path}));	// Add the path to the response so it's easy to program the sidenav
+	});
+}
+
+buildRoute('/',				'homepage', 	{ title: 'Welcome', homepage: true });
+buildRoute('/setup-project',				'setup-project', 	{ title: 'Set up project' });
+
+
+
+module.exports = router
